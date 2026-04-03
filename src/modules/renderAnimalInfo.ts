f8e1583216f7aef78f6f0a0cdce383317da62ce3
Cebellina <emma.andersson.se@outlook.com>
2026-04-03 03:36:34 +0200

    added comments after data analysis in renderAnimalInfo.ts

--- src/modules/renderAnimalInfo.ts
+++ src/modules/renderAnimalInfo.ts
@@ -1,27 +1,41 @@
+// Obs: Skills: 12 av 15 djur. Saknar skills: Nemo (fish), Molly (mole) och Penny (pinguin).
+// Obs: Oscar (owl), Polly (parrot), Penny (penguin) och Molly (mole) har 2 strängar med employments varav en av dem har end date. Det betyder att de ej jobbar.
+// Alla andra djur har 1 employment som inte har end date, vilket betyder att de jobbar.
+// Alla djurs namn börjar på samma bokstav som deras djurtyp, förutom Luna (cat), Daisy (cow) och Nemo (fish).
+
+
 //Titta i index.html och hitta den div där info om varje djur ska skrivas ut.
 //Typa upp funktionens parameter
 
 export default function renderAnimalInfo(animal) {
+ 
   //=====================================================//
   //Rendera ut bilden på djuret
   //Skriv ut djurets namn och typ av djur. Följ det här formatet: "Gina the Giraffe"
+  
   //=====================================================//
   //=====================================================//
+  
   //Skriv ut djurets jobb och om djuret är anställd just nu eller inte.
   //Följ formatet: "Trash Analyst - Currently (not) employed"
   //Använd template literals
+  
   //=====================================================//
   //=====================================================//
+  
   //Skriv ut djurets ålder (age). EJ året de är födda. Utan hur gamla de är.
   //Följ formatet:  Age: 9 years old.
   //Använd template literals
   //Bonus om du skriver ut rubriken "Age" med CSS.
+  
   //=====================================================//
   //=====================================================//
+  
   //Skriv ut en lista på djurets färdigheter
   //Följ formatet:    Skills:
   //                  Flying
   //                  Eating
+  
   //Bonus om du skriver ut rubriken "Skills" med CSS.
   //=====================================================//
 }
